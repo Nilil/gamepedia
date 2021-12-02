fa2bc7523980bff7d7002c3472e8e96b1641fa41
Nilil <nilthonec@gmail.com>
2021-12-02 16:05:10 -0600

    Bootstrap and sass added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import './scss/_custom.scss';
+
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>hi</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
