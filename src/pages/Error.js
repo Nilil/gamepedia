3b3986b3bb97f31322ecf0e1ab530cccdd2f7688
Nilil <nilthonec@gmail.com>
2021-12-03 15:43:31 -0600

    Folders and routes added

---
+++ src/pages/Error.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Error = () => {
+  return(
+    <h1>Ocurrio un error</h1>
+  )
+};
+
+export {Error};
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+
+const Home = () => {
+  return(
+    <Fragment>
+      <h1>Gamepedia</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis consequuntur laborum autem velit error quis inventore impedit quae labore! Repudiandae iusto harum iste ipsam molestias. Home unde distinctio porro quaerat!</p>
+    </Fragment>
+  )
+};
+
+export {Home};
